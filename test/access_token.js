a0c7627d49fce82d207fb6858e2b9348bb729e02
Mikael Brevik <mikaelbre@gmail.com>
2014-07-31 07:45:42 +0200

    Fixes mix of indentation types between tabs and spaces

--- test/access_token.js
+++ test/access_token.js
@@ -1,6 +1,6 @@
 var credentials = { clientID: 'client-id', clientSecret: 'client-secret', site: 'https://example.org' },
     OAuth2 = require('./../lib/simple-oauth2.js')(credentials),
-		qs = require('querystring'),
+    qs = require('querystring'),
     nock = require('nock');
 
 var request, result, token, error;
@@ -8,49 +8,49 @@ var request, result, token, error;
 
 describe('OAuth2.AccessToken',function() {
 
-	beforeEach(function(done) {
-		var params = { 'code': 'code', 'redirect_uri': 'http://callback.com', 'grant_type': 'authorization_code', 'client_id': 'client-id', 'client_secret': 'client-secret' };
-		request = nock('https://example.org:443').post('/oauth/token', qs.stringify(params)).replyWithFile(200, __dirname + '/fixtures/access_token.json');
-		done();
-	})
+  beforeEach(function(done) {
+    var params = { 'code': 'code', 'redirect_uri': 'http://callback.com', 'grant_type': 'authorization_code', 'client_id': 'client-id', 'client_secret': 'client-secret' };
+    request = nock('https://example.org:443').post('/oauth/token', qs.stringify(params)).replyWithFile(200, __dirname + '/fixtures/access_token.json');
+    done();
+  })
 
-	beforeEach(function(done) {
-		var params = { 'code': 'code', 'redirect_uri': 'http://callback.com' }
-		OAuth2.AuthCode.getToken(params, function(e, r) {
-			error = e; result = r; done();
-		})
-	})
+  beforeEach(function(done) {
+    var params = { 'code': 'code', 'redirect_uri': 'http://callback.com' }
+    OAuth2.AuthCode.getToken(params, function(e, r) {
+      error = e; result = r; done();
+    })
+  })
 
-	beforeEach(function(done) {
-		token = OAuth2.AccessToken.create(result);
-		done();
-	});
+  beforeEach(function(done) {
+    token = OAuth2.AccessToken.create(result);
+    done();
+  });
 
-	describe('#create',function() {
+  describe('#create',function() {
 
-		it('creates an access token',function() {
-			token.should.have.property('token');
-		});
-	});
+    it('creates an access token',function() {
+      token.should.have.property('token');
+    });
+  });
 
 
-	describe('when not expired', function() {
+  describe('when not expired', function() {
 
-		it('returns false',function() {
-			token.expired().should.be.false
-		});
-	});
+    it('returns false',function() {
+      token.expired().should.be.false
+    });
+  });
 
-	describe('when expired', function() {
+  describe('when expired', function() {
 
-		beforeEach(function(done) {
-			token.token.expires_at = Date.yesterday();
-			done();
-		});
+    beforeEach(function(done) {
+      token.token.expires_at = Date.yesterday();
+      done();
+    });
 
-		it('returns false',function() {
-			token.expired().should.be.true
-		});
+    it('returns false',function() {
+      token.expired().should.be.true
+    });
     });
     describe('when refreses token', function() {
 
