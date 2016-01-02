e1b96780f5ac0c533053803d71c149acf4e00d2b
Jonathan Samines <jn.samines@gmail.com>
2016-01-01 21:56:25 -0600

    Fixed test style problems

--- test/password.js
+++ test/password.js
@@ -1,47 +1,45 @@
 var credentials = { clientID: 'client-id', clientSecret: 'client-secret', site: 'https://example.org' },
-    oauth2 = require('./../index.js')(credentials),
-    qs = require('querystring'),
-    nock = require('nock');
+  oauth2 = require('./../index.js')(credentials),
+  qs = require('querystring'),
+  nock = require('nock');
 
 var request,
-    result, resultPromise,
-    error, errorPromise,
-    tokenParams = { 'username': 'alice', 'password': 'secret' },
-    oauthParams = { 'username': 'alice', 'password': 'secret', 'grant_type': 'password', 'client_id': 'client-id', 'client_secret': 'client-secret' };
+  result, resultPromise,
+  error, errorPromise,
+  tokenParams = { 'username': 'alice', 'password': 'secret' },
+  oauthParams = { 'username': 'alice', 'password': 'secret', 'grant_type': 'password', 'client_id': 'client-id', 'client_secret': 'client-secret' };
 
-describe('oauth2.password',function() {
-
-  describe('#getToken',function() {
-    beforeEach(function(done) {
+describe('oauth2.password', function () {
+  describe('#getToken', function () {
+    beforeEach(function () {
       request = nock('https://example.org:443')
         .post('/oauth/token', qs.stringify(oauthParams))
         .times(2)
         .replyWithFile(200, __dirname + '/fixtures/access_token.json');
-      done();
-    })
+    });
 
-    beforeEach(function(done) {
-      oauth2.password.getToken(tokenParams, function(e, r) {
+    beforeEach(function (done) {
+      oauth2.password.getToken(tokenParams, function (e, r) {
         error = e; result = r; done();
-      })
-    })
+      });
+    });
 
     beforeEach(function (done) {
       return oauth2.password.getToken(tokenParams)
-      .then(function (r) { resultPromise = r; })
-      .catch(function (e) { errorPromise = e; })
-      .finally(done)
-    })
+        .then(function (r) { resultPromise = r; })
+        .catch(function (e) { errorPromise = e; })
+        .finally(done);
+    });
 
-    it('makes the HTTP request', function() {
+    it('makes the HTTP request', function () {
       request.isDone();
     });
 
-    it('returns an access token as result of callback api',function() {
+    it('returns an access token as result of callback api', function () {
       result.should.have.property('access_token');
     });
 
-    it('returns an access token as result of promises api', function() {
+    it('returns an access token as result of promises api', function () {
       resultPromise.should.have.property('access_token');
     });
   });
