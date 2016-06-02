d415621e10b71b3b5e8017d7cd104bdbdbb01e78
Chris Bargren <cbargren@gmail.com>
2016-06-02 13:33:33 -0700

    Fixing tests

--- test/access_token.js
+++ test/access_token.js
@@ -99,12 +99,12 @@ describe('oauth2.accessToken', function () {
 
     it('returns a new oauth2.accessToken as result of callback api', function () {
       result.should.not.be.equal(global);
-      result.token.should.have.property('access_token');
+      result.should.have.property('access_token');
     });
 
     it('returns a new oauth2.accessToken as result of promise api', function () {
       result.should.not.be.equal(global);
-      resultPromise.token.should.have.property('access_token');
+      resultPromise.should.have.property('access_token');
     });
   });
 
@@ -137,11 +137,11 @@ describe('oauth2.accessToken', function () {
     });
 
     it('returns a new oauth2.accessToken as result of callback api', function () {
-      result.token.should.have.property('access_token');
+      result.should.have.property('access_token');
     });
 
     it('returns a new oauth2.accessToken as result of promise api', function () {
-      resultPromise.token.should.have.property('access_token');
+      resultPromise.should.have.property('access_token');
     });
   });
 
