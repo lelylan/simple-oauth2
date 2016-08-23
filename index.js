97842601ccfb13a8e80323e75f9736763fe6dc29
Jonathan Samines <jn.samines@gmail.com>
2016-08-22 23:29:13 -0600

    Added joi object validation.

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 'use strict';
 
-const utils = require('./lib/utils');
-const defaults = require('./lib/config');
+const joi = require('joi');
 const authCodeModule = require('./lib/client/auth-code');
 const passwordModule = require('./lib/client/password');
 const clienteCredentialsModule = require('./lib/client/client');
 const accessTokenModule = require('./lib/client/access-token');
 
+const optionsSchema = joi
+  .object()
+  .keys({
+    clientID: joi.string().required(),
+    clientSecret: joi.string().required(),
+    site: joi.string().required(),
+    tokenPath: joi.string().default('/oauth/token'),
+    authorizationPath: joi.string().default('/oauth/authorize'),
+    revocationPath: joi.string().default('/oauth/revoke'),
+    useBasicAuthorizationHeader: joi.boolean().default(true),
+    clientSecretParameterName: joi.string().default('client_secret'),
+    useBodyAuth: joi.string().default(true),
+  });
+
 module.exports = function (options) {
-  const config = utils.configure(options, defaults);
+  const opts = Object.assign({}, options || {});
+  const validOptions = joi
+    .attempt(opts, optionsSchema, 'Invalid options provided to simple-oauth2.');
 
   return {
-    authCode: authCodeModule(config),
-    password: passwordModule(config),
-    client: clienteCredentialsModule(config),
-    accessToken: accessTokenModule(config),
+    authCode: authCodeModule(validOptions),
+    password: passwordModule(validOptions),
+    client: clienteCredentialsModule(validOptions),
+    accessToken: accessTokenModule(validOptions),
   };
 };
